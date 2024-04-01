d04cb14833ea6d8bdbac06239d6e560e96343043
syedshariqueee <syedshariqueee@gmail.com>
2024-04-01 16:57:22 +0530

    final

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -12,14 +12,14 @@ const Pagination = () => {
     <div className='flex items-center gap-x-3 w-11/12 max-w-2xl mx-auto'>
       {
         page > 1 &&
-        <button className='border-2 border-gray-300 py-1 px-4 rounded-md' onClick={() => handlerPageChange(page - 1)}>Previous</button>
+        <button className='border-2 bg-red-600 text-white border-gray-300 py-1 px-4 rounded-md' onClick={() => handlerPageChange(page - 1)}>Previous</button>
       }
       {
         page < totalPages &&
-        <button className='border-2 border-gray-300 py-1 px-4 rounded-md' onClick={() => handlerPageChange(page + 1)}>Next</button>
+        <button className='border-2 border-gray-300 text-white bg-green-600 py-1 px-4 rounded-md' onClick={() => handlerPageChange(page + 1)}>Next</button>
       }
 
-      <p className='text-sm font-semibold ml-auto'>page {page} of {totalPages}</p>
+      <p className='border-2 border-gray-300 text-white  font-bold bg-blue-400 py-1 px-4 rounded-md text-sm ml-auto'>Page {page} of {totalPages}</p>
     </div>
   </div>
   )
